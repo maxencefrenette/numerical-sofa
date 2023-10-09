7c6e86b21f07d1f86346d0a7e387607bb2d86db0
Maxence Frenette <maxence.frenette@gmail.com>
2023-10-09 12:51:54 -0700

    Add eframe and egui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use eframe::egui;
+
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(1280.0, 720.0)),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "Numerical Sofa Results Viewer",
+        options,
+        Box::new(|_| Box::<MyApp>::default()),
+    )
+}
+
+struct MyApp {}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let p = ui.painter();
+            p.circle_filled(egui::pos2(300.0, 300.0), 200.0, egui::Color32::RED);
+        });
+    }
 }
